f4789a77ce8f70dcc191368c0098b463354fbec1
Damian Obara <damianoobara@gmail.com>
2020-04-21 17:09:24 +0200

    parsowanie host:port

--- testhttp_raw.c
+++ testhttp_raw.c
@@ -15,18 +15,26 @@
 #include "err.h"
 
 #define BUFFER_SIZE      1024
+#define HOST_SIZE      1024
+#define PORT_SIZE      32
+
 
 static const char bye_string[] = "BYE";
 
 int main (int argc, char *argv[]) {
+
+    printf("Hello");
+
     int rc;
     int sock;
     struct addrinfo addr_hints, *addr_result;
     char line[BUFFER_SIZE];
+    char host[HOST_SIZE];
+    char port[PORT_SIZE];
 
     /* Kontrola dokumentów ... */
-    if (argc != 3) {
-        fatal("Usage: %s host port", argv[0]);
+    if (argc != 4) {
+        fatal("Usage: %s connectio-host:connection-port file resource-adress", argv[0]);
     }
 
     sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -41,7 +49,24 @@ int main (int argc, char *argv[]) {
     addr_hints.ai_socktype = SOCK_STREAM;
     addr_hints.ai_protocol = IPPROTO_TCP;
 
-    rc =  getaddrinfo(argv[1], argv[2], &addr_hints, &addr_result);
+
+    //Parse host:port
+    char *delimiter = strchr(argv[1],':');
+    int host_len = delimiter - argv[1];
+    strncpy(host, argv[1], host_len);
+    host[host_len] = '\0';
+    printf("%s\n", host);
+    strcpy(port, argv[1] + host_len + 1);
+    printf("%s\n", port);
+
+    //Open file
+    FILE *cookies_file;
+    if ((cookies_file = fopen(argv[2], "r"))){
+        fclose(cookies_file);
+        fatal("Nie ma takiego pliku %s", argv[2]);
+    }
+
+    rc =  getaddrinfo(host, port, &addr_hints, &addr_result);
     if (rc != 0) {
         fprintf(stderr, "rc=%d\n", rc);
         syserr("getaddrinfo: %s", gai_strerror(rc));
